b77459d5048eeb9b2e38e8fac8782ca4aab4db94
GoodBebra <log56790@yandex.ru>
2025-01-31 17:55:52 +0700

    go go go1

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -8,26 +8,85 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _obscureText = true ;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Column(
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Container(
-              margin: const EdgeInsets.fromLTRB(20, 121, 20, 275),
-              child: const Column(children: [
-              Text("Привет!", style: TextStyle(fontSize: 32),),
-              Text("Заполните Свои данные Или", style: TextStyle(fontSize: 16, color: Color(0xFF707B81)) ),
-              Text('продолжите через социальные медиа', style: TextStyle(fontSize: 16, color: Color(0xFF707B81))),
-            ]
-            )
-            ),
-            SizedBox(
-            ),
-            Container()
+            _buildHeader(),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildHeader() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 121),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            "Привет!",
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8,)
+          Text('Заполните Свои Данные Или\nПродолжите Через Социальные Медиа',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 16, color: Color(0xFF707B81)),
+          ),
+        ],
+        ),
+      );
+  }
+
+  Widget _buildForm() {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      
+    ],
+    );
+  }
+
+  Widget _buildLabel(String text) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12),
+      child: Text(text,
+      style: const TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+      ),
+    );
+  }
+
+  Widget _buildTextField({required TextEditingController controller, required String hintText}) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: const TextStyle(
+          color: Color(0xFF6A6A6A),
+          fontWeight: FontWeight.normal,
+          fontSize: 14,
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: BorderSide.none
+        ),
+        filled: true,
+        fillColor: const Color(0xFFF7F7F9),
       ),
     );
   }
