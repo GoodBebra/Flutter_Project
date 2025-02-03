304e2c69422870fad00c4556a735ac5655f01e31
GoodBebra <log56790@yandex.ru>
2025-02-03 17:22:31 +0700

    New_Commit_03

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home_screen.dart';
+
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
 
@@ -8,9 +10,9 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
-final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  bool _obscureText = true ;
+  bool _obscureText = true;
 
   @override
   void dispose() {
@@ -44,34 +46,68 @@ final TextEditingController _emailController = TextEditingController();
             "Привет!",
             style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
           ),
-          const SizedBox(height: 8,),
-          Text('Заполните Свои Данные Или\nПродолжите Через Социальные Медиа',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 16, color: Color(0xFF707B81)),
+          const SizedBox(
+            height: 8,
           ),
+          const Text(
+            'Заполните Свои Данные Или\nПродолжите Через Социальные Медиа',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 16, color: Color(0xFF707B81)),
+          ),
+          const SizedBox(
+            height: 35,
+          ),
+          _buildForm()
         ],
-        ),
-      );
+      ),
+    );
   }
 
   Widget _buildForm() {
-    return Column(crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      
-    ],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildLabel("Email"),
+        _buildTextField(
+            controller: _emailController, hintText: "xyz@gmail.com"),
+        const SizedBox(
+          height: 26,
+        ),
+        _buildLabel("Пароль"),
+        _buildPasswordField(),
+        const SizedBox(
+          height: 8,
+        ),
+        Align(
+          alignment: Alignment.centerRight,
+          child: GestureDetector(
+            onTap: () {},
+            child: const Text(
+              "Востановить",
+              style: TextStyle(color: Color(0xFF707B81), fontSize: 12),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 24,
+        ),
+        _buildLoginButton(),
+      ],
     );
   }
 
   Widget _buildLabel(String text) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 12),
-      child: Text(text,
-      style: const TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+      child: Text(
+        text,
+        style: const TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
       ),
     );
   }
 
-  Widget _buildTextField({required TextEditingController controller, required String hintText}) {
+  Widget _buildTextField(
+      {required TextEditingController controller, required String hintText}) {
     return TextField(
       controller: controller,
       decoration: InputDecoration(
@@ -82,11 +118,82 @@ final TextEditingController _emailController = TextEditingController();
           fontSize: 14,
         ),
         border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide.none),
+        filled: true,
+        fillColor: const Color(0xFFF7F7F9),
+      ),
+    );
+  }
+
+  Widget _buildPasswordField() {
+    return TextField(
+      controller: _passwordController,
+      obscureText: _obscureText,
+      decoration: InputDecoration(
+        hintText: "••••••••",
+        hintStyle: const TextStyle(
+          color: Color(0xFF6A6A6A),
+          fontSize: 14,
+        ),
+        border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(15),
-          borderSide: BorderSide.none
+          borderSide: BorderSide.none,
         ),
         filled: true,
         fillColor: const Color(0xFFF7F7F9),
+        suffixIcon: IconButton(
+          icon: Image.asset(
+            _obscureText
+                ? "assets/images/icons/eye-close.png"
+                : "assets/images/icons/eye-open.png",
+            width: 24,
+            height: 24,
+          ),
+          onPressed: () {
+            setState(() {
+              _obscureText = !_obscureText;
+            });
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoginButton() {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const HomeScreen()),
+          );
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color(0xFF48B2E7),
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 14),
+        ),
+        child: const Text("Войти"),
+      ),
+    );
+  }
+
+  Widget _buildFooter() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 50),
+      child: Center(
+        child: GestureDetector(
+          onTap: () {},
+          child: const Text(
+            "Вы впервые? Создать пользователя",
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
       ),
     );
   }
